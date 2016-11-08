755eb71ffe256b61f35284b0feeba7189d0e994e
Veit Rautmann <veit.rautmann@web.de>
2016-11-08 10:43:02 +0100

    Kleine Aenderung in latex-output

--- 1.2-BETA/output_latex.cpp
+++ 1.2-BETA/output_latex.cpp
@@ -82,7 +82,7 @@ void print_latex_source(string &output_path, string stat_id, string station,
     file_out << "   \\includegraphics[width=6cm]{S:/User/Ra/DiesDas/Unterlagen/Logo/Logo.jpg}\\\\\n";
     file_out << "   \\today\n";
     file_out << "}\n";
-    file_out << "\\ofoot{\\footnotesize{Erstellt von M.Sc. Met. V. Rautmann}}\n\n";
+    file_out << "\\ofoot{\\footnotesize{Erstellt durch M.Sc. Veit Rautmann}}\n\n";
     file_out << "\\section*{Bestimmung des repräsentativen Jahres}\n";
     file_out << "Für eine ausgewählte Messstation wird auf Basis einer "\
 				"mehrjährigen Stunden-Zeitreihe ein repräsentatives Jahr"\
@@ -145,7 +145,7 @@ void print_latex_source(string &output_path, string stat_id, string station,
 	file_out << "\\ohead{\n";
 	file_out << "   \\includegraphics[width=6cm]{S:/User/Ra/DiesDas/Unterlagen/Logo/Logo.jpg}\n";
 	file_out << "   }\n";
-	file_out << "\\ofoot{\\footnotesize{Erstellt von M.Sc. Met. V. Rautmann}}\n\n";
+	file_out << "\\ofoot{\\footnotesize{Erstellt durch M.Sc. Veit Rautmann}}\n\n";
 	file_out << "\\begin{table}[!htb]\n";
 	file_out << "   \\renewcommand{\\arraystretch}{1.4}\n";
 	file_out << "   \\centering\n";
